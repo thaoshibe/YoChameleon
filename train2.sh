18c2ba937d20bfac55e2e2967c83200223424f1f
thaoshibe <thao.nguyen@wisc.edu>
2024-10-13 14:34:36 -0500

    clean training process

--- train2.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-24.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-32.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-40.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-48.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-56.yaml
-CUDA_VISIBLE_DEVICES=0,1 python train_wandb.py --config config/frozen-64.yaml
