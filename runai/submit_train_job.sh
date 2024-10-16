7d712fc413fa09748ccc702a489dd20ac917ccba
thaoshibe <thao.nguyen@wisc.edu>
2024-10-16 18:20:40 -0500

    update bash script

--- runai/submit_train_job.sh
+++ runai/submit_train_job.sh
@@ -37,5 +37,5 @@ runai submit --large-shm \
     -l research_jack_id=$RESEARCH_JACK_ID \
     -l activity_type=focused_research \
     -e WANDB_API_KEY=$WANDB_API_KEY \
-    --command -- bash -c '"cd '${SCRIPT_DIR}'; umask 007; bash ./launch_train.sh;"'
+    --command -- bash -c '"cd '${SCRIPT_DIR}'; umask 007; bash ./launch_train2.sh;"'
     # --command --working-dir=/sensei-fs/users/thaon/code/YoChameleon -- bash -c "/sensei-fs/users/thaon/code/YoChameleon/bash launch_train.sh"
