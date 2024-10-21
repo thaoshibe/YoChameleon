9c4d651f36f6dfe2ee00d6a3fda390a12cfe4207
thaoshibe <thao.nguyen@wisc.edu>
2024-10-20 22:10:41 -0500

    1000 negative bash scrips

--- runai/delete.sh
+++ runai/delete.sh
@@ -6,19 +6,13 @@ PROJECT="ilo-noquota-p4de"
 
 # List of job names to delete
 jobs=(
-  "yj-idea-1016-1821"
-  "yj-idea-1015-1943"
-  "yh-idea-1015-1851"
-  "yj-idea-1015-1828"
-  "smaller-lr-1014-2137"
-  "test-1014-1659"
-  "test-1014-1042"
-  "test1-1014"
-  "thao1020"
-  "thao1023"
-  "debug1019"
-  "yj-idea-1016-1809"
-  "yj-idea-quota-1016-1821"
+  "yollava-setting-1020-1403"
+  "yollava-setting-1020-1350"
+  "yollava-setting-1020-1343"
+  "yollava-setting-1020-1334"
+  "yollava-setting-1020-1329"
+  "prefix-positive-only-1020-1310"
+  "prefix-positive-only-1020-1248"
 )
 
 # Loop through each job and delete it
