4b7aabc140fe00c6024acbaf7ffcfbba87103b03
thaoshibe <thao.nguyen@wisc.edu>
2024-10-27 17:10:30 -0500

    minor bugs in launch scripts

--- scripts/launch_1000disjoin.sh
+++ scripts/launch_1000disjoin.sh
@@ -1,40 +1,40 @@
-# USER="$(whoami)"
-# echo "USER"
-# echo $USER
+USER="$(whoami)"
+echo "USER"
+echo $USER
 
-# WORKING_FOLDER="/mnt/localssd/code"
-# DATA_ZIP_FILE="/sensei-fs/users/thaon/data/yochameleon-data.zip"
-# CODE_FOLDER="/sensei-fs/users/thaon/code/YoChameleon"
+WORKING_FOLDER="/mnt/localssd/code"
+DATA_ZIP_FILE="/sensei-fs/users/thaon/data/yochameleon-data.zip"
+CODE_FOLDER="/sensei-fs/users/thaon/code/YoChameleon"
 
-# if [ ! -d "/home/user/" ]; then
-#   sudo mkdir /home/user/
-# fi
+if [ ! -d "/home/user/" ]; then
+  sudo mkdir /home/user/
+fi
 
-# if [ ! -d "/home/$USER" ]; then
-#   sudo mkdir /home/$USER
-# fi
+if [ ! -d "/home/$USER" ]; then
+  sudo mkdir /home/$USER
+fi
 
-# sudo chmod 777 -R /home/
+sudo chmod 777 -R /home/
 
-# echo "Launching training script"
-# mkdir -p $WORKING_FOLDER
-# cd $WORKING_FOLDER
-# cp -r $CODE_FOLDER $WORKING_FOLDER
+echo "Launching training script"
+mkdir -p $WORKING_FOLDER
+cd $WORKING_FOLDER
+cp -r $CODE_FOLDER $WORKING_FOLDER
+
+cd $WORKING_FOLDER/YoChameleon
+bash scripts/install.sh
+
+mkdir -p $WORKING_FOLDER/data
+cd $WORKING_FOLDER/data
+cp -r $DATA_ZIP_FILE $WORKING_FOLDER/data
+unzip $WORKING_FOLDER/data/yochameleon-data.zip
 
 # cd $WORKING_FOLDER/YoChameleon
-# bash scripts/install.sh
+export WANDB_API_KEY="563710e55fec9aac8f27c7ab80cfed931a2096f5"
 
-# mkdir -p $WORKING_FOLDER/data
-# cd $WORKING_FOLDER/data
-# cp -r $DATA_ZIP_FILE $WORKING_FOLDER/data
-# unzip $WORKING_FOLDER/data/yochameleon-data.zip
-
-# # cd $WORKING_FOLDER/YoChameleon
-# export WANDB_API_KEY="563710e55fec9aac8f27c7ab80cfed931a2096f5"
-
-# # Train script
-# cd $WORKING_FOLDER/YoChameleon
-# ("thao" "yuheng" "thuytien" "viruss" "ciin" "khanhvy" "oong" "willinvietnam" "denisdang" "phuc-map")
+# Train script
+cd $WORKING_FOLDER/YoChameleon
+("thao" "yuheng" "thuytien" "viruss" "ciin" "khanhvy" "oong" "willinvietnam" "denisdang" "phuc-map")
 
 CUDA_VISIBLE_DEVICES=0,1 python train.py --config ./config/1000disjoin.yaml --sks_name "thao" &
 CUDA_VISIBLE_DEVICES=2,3 python train.py --config ./config/1000disjoin.yaml --sks_name "yuheng" &
